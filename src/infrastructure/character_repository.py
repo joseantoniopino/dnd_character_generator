317c991528fc3da4ae73bfca3a23f6e074146f06
Jose A <josanpino@gmail.com>
2023-01-30 22:29:58 +0100

    Refactor character_repository_interface

--- src/infrastructure/character_repository.py
+++ src/infrastructure/character_repository.py
@@ -1,18 +1,25 @@
 # character_repository.py
+from abc import ABC
 
-class CharacterRepository:
-    def save(self, character):
-        raise NotImplementedError
+from src.infrastructure.interfaces.character_repository_interface import CharacterRepositoryInterface
 
 
-class MySQLCharacterRepository(CharacterRepository):
+class MySQLCharacterRepository(CharacterRepositoryInterface, ABC):
     def __init__(self, connection):
         self.connection = connection
 
-    def save(self, character):
+    def save_character(self, character):
         cursor = self.connection.cursor()
         sql = "INSERT INTO characters (name, class, race, background, age) VALUES (%s, %s, %s, %s, %s)"
         values = (character.name, character.char_class, character.race, character.background, character.age)
         cursor.execute(sql, values)
         self.connection.commit()
         cursor.close()
+
+    def get_character(self, character_id):
+        cursor = self.connection.cursor()
+        sql = "SELECT * FROM characters WHERE id = %s"
+        cursor.execute(sql, character_id)
+        result = cursor.fetchone()
+        cursor.close()
+        return result
---
+++ src/infrastructure/interfaces/character_repository_interface.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+from src.domain.character import Character
+
+
+class CharacterRepositoryInterface(ABC):
+    @abstractmethod
+    def save_character(self, character: Character) -> None:
+        pass
+
+    @abstractmethod
+    def get_character(self, character_id: int) -> Character:
+        pass
